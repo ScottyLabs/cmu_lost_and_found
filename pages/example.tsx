adbddcbc1a8321059d85f6e80478776baa7d84e6
Richard G <rwguo@andrew.cmu.edu>
2020-09-15 22:21:55 -0500

    setup done

---
+++ pages/example.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Example() {
+  return (
+    <>
+      <h1>Hello there</h1>
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>
+    </>
+  );
+}
