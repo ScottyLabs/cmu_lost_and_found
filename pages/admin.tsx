ac13eb98caf4ac4e0254677b27e8eda882287753
jackiey321 <67326858+jackiey321@users.noreply.github.com>
2020-10-24 18:20:47 -0400

    added admin page

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,12 +1,17 @@
+import Head from "next/head";
 import Link from "next/link";
+import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 export default function Example() {
   return (
-    <>
-      <h1>Hello there</h1>
-      <Link href="/">
-        <a>Back to Home</a>
-      </Link>
-    </>
+    <div className="container">
+      <Head>
+        <title>CMU Lost and Found</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Admin Page</h1>
+      <Button><Link href="/"><a>Back to Home</a></Link></Button>
+    </div>
   );
 }
